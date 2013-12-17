f4755d7164464eaa87076471a522e8024957b11e
Phil Thompson <phil@electricvisions.com>
2013-12-17 07:19:31 +0000

    initial commit - basic expect().to.receive syntax with expect.js support

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+  var PORT = 80;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**/*.js', 'spec/**/*.js']
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'progress',
+
+          // Ensure tests are reloaded when in same process (i.e. with spawn: false)
+          clearRequireCache: true
+        },
+        src: ['spec/**/*.js']
+      }
+    },
+
+    watch: {
+      options: {
+        spawn: false,
+        interrupt: true
+      },
+      hint: {
+        files: ['lib/**/*.js', 'spec/**/*.js'],
+        tasks: ['jshint']
+      },
+      mocha: {
+        files: ['lib/**/*.js', 'spec/**/*.js'],
+        tasks: ['mochaTest']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+};
---
+++ spec/main_spec.js
@@ -0,0 +1,40 @@
+var expect = require('../lib/fake_expect.js'),
+    fake = require('../lib/fake.js');
+
+describe('fake', function () {
+  describe('when closed', function () {
+    var logger, subject;
+
+    beforeEach(function () {
+      logger = fake('Logger');
+      subject = {
+        close: function close() {
+          logger.account_closed(this);
+        }
+      };
+      subject.logger = logger;
+    });
+
+    it('logs an account closed message', function () {
+      expect(logger).to.receive('account_closed', subject);
+      subject.close();
+      logger.verify();
+    });
+
+    it('fails to log an account closed message', function () {
+      expect(logger).to.receive('account_closed', subject);
+      expect(logger.verify).to.throwError(/Expected Logger.account_closed\(\[object Object\]\) to be called, but was not called./);
+    });
+
+    it('does not log an account closed message', function () {
+      expect(logger).to.not.receive('account_closed', subject);
+      logger.verify();      
+    });
+
+    it('fails to not log an account closed message', function () {
+      expect(logger).to.not.receive('account_closed', subject);
+      subject.close();
+      expect(logger.verify).to.throwError(/Expected Logger.account_closed\(\[object Object\]\) to not be called, but was called./);
+    });
+  });
+});
