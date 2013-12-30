7aa32991cde3976d7c72add245edf1bf268580e3
Phil Thompson <phil@electricvisions.com>
2013-12-30 16:20:21 +0000

    Filled out README. Added verifyAll, once, twice, multiple return values

--- README.md
+++ README.md
@@ -1,23 +1,54 @@
 # MiniFake
 
-Simple fakes with contract tests inpired by RSpec.
+Simple fakes with contract tests inspired by RSpec, Bogus and work by JB Rainsberger.
+
+## Synopsis
 
 Currently works with expect.js.
+
+It's simple in that you always use `fake('name')` to create a new fake regardless of whether you're stubbing or mocking.
+
+It extends the expect.js syntax by adding `allow()` to cover stubs as does the RSpec framework.
+
+## Usage
 
     var expect = require('../lib/fake_expect.js'),
         fake = require('../lib/fake.js');
     ...
     var thing = fake('Thing');
 
-    // mocks
-    expect(thing).to.receive('method', param1, param2, ...);
+### mocks
 
-    // stubs
-    allow(thing).to.recevie('another_method').and_return('something');
+    expect(thing).to.receive('method', param1, param2, ...).and_return('something');
+
+### stubs
+
+    allow(thing).to.receive('another_method').and_return('something');
     expect(thing.another_method()).to.equal('something');
 
+### verify expectations
 
-    // verify mock expectations
+Hope to move into an auto afterEach function.
 
     thing.verify();
+    thing.verifyAll(); // verifies all fakes (not just the current one)
 
+### number of times a fake is called
+
+    expect(thing).to.receive('method').once().and_return(1);
+
+### return something different on each call
+
+    expect(thing).to.receive('method').twice().and_return(1, 2);
+
+### Second receive overrides first
+
+    expect(thing).to.receive('method').and_return(1);
+    expect(thing).to.receive('method').and_return(2);
+
+    thing.method(); // => 2
+
+## TODO
+
+* Different paramater calls should be separate expectations.
+* Identify missing contract tests (i.e. mocks used without corresponding test)
--- spec/fake_spec.js
+++ spec/fake_spec.js
@@ -5,38 +5,116 @@ var expect = require('../lib/fake_expect.js'),
 
 describe('fake', function () {
   describe('#receive', function () {
-    var logger, subject;
+    var logger, account;
 
     beforeEach(function () {
       logger = fake('Logger');
-      subject = {
+      account = {
         close: function close() {
           logger.account_closed(this);
         }
       };
-      subject.logger = logger;
+      account.logger = logger;
     });
 
     it('logs an account closed message', function () {
-      expect(logger).to.receive('account_closed', subject);
-      subject.close();
+      expect(logger).to.receive('account_closed', account);
+      account.close();
       logger.verify();
     });
 
-    it('fails to log an account closed message', function () {
-      expect(logger).to.receive('account_closed', subject);
+    it('fails when expecting to log an account closed message', function () {
+      expect(logger).to.receive('account_closed', account);
       expect(logger.verify).to.throwError(/Expected Logger.account_closed\(\[object Object\]\) to be called, but was not called./);
     });
 
     it('does not log an account closed message', function () {
-      expect(logger).to.not.receive('account_closed', subject);
+      expect(logger).to.not.receive('account_closed', account);
       logger.verify();
     });
 
-    it('fails to not log an account closed message', function () {
-      expect(logger).to.not.receive('account_closed', subject);
-      subject.close();
+    it('fails when expecting not to log an account closed message', function () {
+      expect(logger).to.not.receive('account_closed', account);
+      account.close();
       expect(logger.verify).to.throwError(/Expected Logger.account_closed\(\[object Object\]\) to not be called, but was called./);
+    });
+  });
+
+  it('second receive overrides first', function () {
+    var subject = fake();
+
+    expect(subject).to.receive('method').and_return(1);
+    expect(subject).to.receive('method').and_return(2);
+
+    expect(subject.method()).to.equal(2);
+  });
+
+  describe('once', function () {
+    var subject = fake();
+
+    beforeEach(function () {
+      expect(subject).to.receive('method').once().and_return(1);
+    });
+
+    it('calls fake once', function () {
+      subject.method();
+      subject.verify();
+    });
+
+    it('fails when called twice', function () {
+      subject.method();
+      subject.method();
+      expect(subject.verify).to.throwError(/Expected Fake.method\(\) to be called once, but was called twice./);
+    });
+
+    it('returns a value', function () {
+      expect(subject.method()).to.equal(1);
+    });
+  });
+
+  describe('twice', function () {
+    var subject = fake();
+
+    beforeEach(function () {
+      expect(subject).to.receive('method').twice().and_return(1);
+    });
+
+    it('calls fake twice', function () {
+      subject.method();
+      subject.method();
+      subject.verify();
+    });
+
+    it('fails when only called once', function () {
+      subject.method();
+      expect(subject.verify).to.throwError(/Expected Fake.method\(\) to be called twice, but was called once./);
+    });
+
+    it('returns a value', function () {
+      expect(subject.method()).to.equal(1);
+      expect(subject.method()).to.equal(1);
+      subject.verify();
+    });
+
+    it('returns different value on subsequent calls', function () {
+      expect(subject).to.receive('method').twice().and_return(1, 2);
+      expect(subject.method()).to.equal(1);
+      expect(subject.method()).to.equal(2);
+      subject.verify();
+    });
+  });
+
+  describe('verifyAll', function () {
+    it('verifies all fakes', function () {
+      var fake1 = fake('Fake1'),
+          fake2 = fake('Fake2');
+
+      expect(fake1).to.receive('method1');
+      expect(fake2).to.receive('method2');
+
+      fake1.method1();
+
+      expect(fake1.verifyAll).to.throwError();
     });
   });
 });
