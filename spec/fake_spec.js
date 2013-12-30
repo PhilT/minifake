5482f20dfec9fbd98563621493b90cc06921426e
Phil Thompson <phil@electricvisions.com>
2013-12-30 23:18:32 +0000

    added entrypoint for expect.js integration

--- spec/fake_spec.js
+++ spec/fake_spec.js
@@ -6,6 +6,9 @@
 // Unfortunately, I can't figure out a way to
 // test this as it seems impossible to override afterEach.
 
+// var fake = require('../lib/fake.js').fake,
+//     expect = require('../lib/fake.js').expect;
+
 // describe('fake', function () {
 //   it('ensure fake is verified', function () {
 //     expect(fake()).to.receive('method');
