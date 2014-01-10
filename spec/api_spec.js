b3bc2024b9f9e2c417da9453f8e2973ee6d309a7
Phil Thompson <phil@electricvisions.com>
2014-01-10 08:36:52 +0000

    added more comprehensive tests for api

--- spec/api_spec.js
+++ spec/api_spec.js
@@ -1,27 +1,54 @@
 'use strict';
 
-var expect = require('../node_modules/expect.js/expect.js'),
+var expect = require('expect.js'),
     api = require('../lib/api.js');
 
 describe('api', function () {
-  describe('setupReceive', function () {
+  describe('.setup', function () {
     var fake,
-        registerCalledWith;
+        registerCalledWith,
+        receive,
+        assertion;
 
     beforeEach(function () {
-      registerCalledWith = '';
       fake = {
-        register: function register(method) {
-          registerCalledWith = method;
+        register: function register(method, args, options) {
+          registerCalledWith = {method: method, args: args, options: options};
         }
       };
+      assertion = {obj: fake};
+      registerCalledWith = {};
     });
 
-    it('returns a receive function that calls register', function () {
-      var receive = api.setup(true),
-          assertion = {obj: fake};
-      receive.call(assertion, 'method');
-      expect(registerCalledWith).to.equal('method');
+    describe('returns receive function and ', function () {
+      beforeEach(function () {
+        receive = api.setup('required', 'property');
+      });
+
+      it('calls register with method', function () {
+        receive.call(assertion, 'method');
+        expect(registerCalledWith.method).to.equal('method');
+      });
+
+      it('calls register arguments', function () {
+        receive.call(assertion, null, 1, 2);
+        expect(registerCalledWith.args).to.eql([1, 2]);
+      });
+
+      it('calls register with correct options', function () {
+        receive.call(assertion);
+        expect(registerCalledWith.options).to.eql({
+          required: 'required',
+          not: undefined,
+          property: 'property'
+        });
+      });
+
+      it('calls register with "not" set when flags on fake set', function () {
+        assertion.flags = {not: 'not'};
+        receive.call(assertion);
+        expect(registerCalledWith.options.not).to.equal('not');
+      });
     });
   });
 });
